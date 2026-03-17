a113cb0425671b5877f3cfb97f166a4ef1439a75
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 19:54:36 +0530

    Update cfar_core.v

--- src/cfar_core.v
+++ src/cfar_core.v
@@ -10,17 +10,17 @@ module cfar_core (
 
     always @(posedge clk) begin
         if (!rst_n) begin
-            threshold <= 14'd100;
-            detect <= 0;
-            bcd_out <= 0;
+            threshold <= 14'd50;   // smaller safe value
+            detect    <= 1'b0;
+            bcd_out   <= 4'd0;
         end else begin
             threshold <= (threshold + signal) >> 1;
 
             if (signal > threshold) begin
-                detect <= 1;
+                detect  <= 1'b1;
                 bcd_out <= 4'd1;
             end else begin
-                detect <= 0;
+                detect  <= 1'b0;
                 bcd_out <= 4'd0;
             end
         end
