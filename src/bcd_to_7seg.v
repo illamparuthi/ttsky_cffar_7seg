9252d755643c4020784315e30b8010e44a2f0fd6
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-17 19:19:30 +0530

    Create bcd_to_7seg.v

---
+++ src/bcd_to_7seg.v
@@ -0,0 +1,14 @@
+module bcd_to_7seg (
+    input [3:0] bcd,
+    output reg [6:0] seg
+);
+
+always @(*) begin
+    case (bcd)
+        4'd0: seg = 7'b1000000;
+        4'd1: seg = 7'b1111001;
+        default: seg = 7'b1111111;
+    endcase
+end
+
+endmodule
