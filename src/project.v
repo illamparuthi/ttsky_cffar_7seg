55715b100a2160fba04a07d5f31632e7cee90fca
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-18 12:23:05 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,3 +1,5 @@
+`default_nettype none
+
 module tt_um_cfar_nobuzzer (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
@@ -9,16 +11,13 @@ module tt_um_cfar_nobuzzer (
     input  wire       rst_n
 );
 
-    // Internal wires
     wire [13:0] adc_data;
-    wire detect_val;
-    wire [3:0] bcd_val;
-    wire [6:0] segments;
+    wire        detect_val;
+    wire [3:0]  bcd_val;
+    wire [6:0]  segments;
 
-    // 14-bit ADC reconstruction: {uio_in[5:0], ui_in[7:0]}
-    assign adc_data = {uio_in[5:0], ui_in}; [cite: 13]
+    assign adc_data = {uio_in[5:0], ui_in};
 
-    // Core CFAR Instance
     cfar_core cfar_inst (
         .clk(clk),
         .rst_n(rst_n),
@@ -27,15 +26,13 @@ module tt_um_cfar_nobuzzer (
         .bcd_out(bcd_val)
     );
 
-    // BCD to Display Instance
     bcd_to_7seg display_inst (
         .bcd(bcd_val),
         .seg(segments)
     );
 
-    // Outputs
-    assign uo_out  = {detect_val, segments}; [cite: 17]
-    assign uio_out = 8'b0; [cite: 19]
-    assign uio_oe  = 8'b0; [cite: 19]
+    assign uo_out  = {detect_val, segments};
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
 endmodule
