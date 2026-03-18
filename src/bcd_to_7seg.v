2296e8c6371a1cf9fda4f3882440fa9a9c8b1c83
illamparuthi <116744508+illamparuthi@users.noreply.github.com>
2026-03-18 12:13:22 +0530

    Update bcd_to_7seg.v

--- src/bcd_to_7seg.v
+++ src/bcd_to_7seg.v
@@ -1,13 +1,12 @@
 module bcd_to_7seg (
     input [3:0] bcd,
-    output reg [6:0] seg // {g,f,e,d,c,b,a}
+    output reg [6:0] seg
 );
     always @(*) begin
         case (bcd)
-            // Segments: g f e d c b a
-            4'd0: seg = 7'b1000000; // Displays '0' [cite: 2]
-            4'd1: seg = 7'b1111001; // Displays '1' [cite: 3]
-            default: seg = 7'b1111111; // All OFF [cite: 3]
+            4'd0:    seg = 7'b1000000; // Displays '0' 
+            4'd1:    seg = 7'b1111001; // Displays '1' [cite: 3]
+            default: seg = 7'b1111111; // All OFF
         endcase
     end
 endmodule
